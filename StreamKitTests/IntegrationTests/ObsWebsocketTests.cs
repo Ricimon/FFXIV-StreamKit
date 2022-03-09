fe9de22e123442453f09a85e97c6e3e16339511c
Ricimon <ricimon25@gmail.com>
2022-03-09 01:49:41 -0800

    Initial commit. Modified clone of Dalamud sample plugin, and some tests of obs-websocket-dotnet

---
+++ StreamKitTests/IntegrationTests/ObsWebsocketTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using OBSWebsocketDotNet;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StreamKitTests.IntegrationTests
+{
+    // OBS with the obs-websocket plugin must be running for these tests to pass.
+    public class ObsWebsocketTests
+    {
+        private IConfiguration Configuration { get; set; }
+
+        private string ObsWebsocketUrl => TestContext.Parameters["ObsWebsocketUrl"];
+        private string ObsWebsocket4Password => Configuration["ObsWebsocket4Password"];
+
+        public ObsWebsocketTests()
+        {
+            var builder = new ConfigurationBuilder().AddUserSecrets<ObsWebsocketTests>();
+            Configuration = builder.Build();
+        }
+
+        [Test]
+        public async Task ConnectToObsWebsocket_ShouldWork()
+        {
+            using var ws = new ClientWebSocket();
+            await ws.ConnectAsync(new(ObsWebsocketUrl), CancellationToken.None);
+            Assert.AreEqual(WebSocketState.Open, ws.State);
+        }
+
+        [Test]
+        public void ObsWebsocketDotnetConnection_ShouldWork()
+        {
+            var obs = new OBSWebsocket();
+            obs.Connect(ObsWebsocketUrl, ObsWebsocket4Password);
+        }
+
+        [Test]
+        public async Task Testing()
+        {
+        }
+    }
+}
